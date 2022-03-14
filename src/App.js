a21986cad8f0a9f86c5a37747fd0e4535013423e
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-03-14 00:39:41 -0300

    Salvando progresso

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 import React from 'react';
+
+
+import{Container, Seçao} from '.styles'
+
+function App () {
+
+return(
+
+    <Container>
+        <div>
+        <ul>
+
+        
+        </ul>
+        </div>
+
+    </Container>
+
+)
+
+}
+
+
+
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+background: #0C0C0C;
+
+`
+
+export const Seçao = styled.li`
+
+
+`
